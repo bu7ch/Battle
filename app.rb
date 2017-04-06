1d65377427b10306aed88264bb4453f9517f7aca
MrRobot <dutchr@live.fr>
2017-04-06 18:29:53 +0200

    add tests & PASS

--- app.rb
+++ app.rb
@@ -2,23 +2,23 @@ require "sinatra/base"
 
 class Battle < Sinatra::Base
 
-# enable :sessions
+enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-     @player_1_name = params[:player_1_name]
-     @player_2_name = params[:player_2_name]
+     session['player_1_name'] = params[:player_1_name]
+     session['player_2_name'] = params[:player_2_name]
+    redirect '/play'
+  end
 
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
-#
-#   get '/play' do
-#     @player_1_name = session[:player_1_name]
-#     @player_2_name = session[:player_2_name]
-#   end
-#
+
 run! if app_file == $0
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,12 +1,21 @@
-# describe Battle do
+describe Battle do
 
-  feature 'Player details' do
-    scenario 'entring names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Emilie'
-      fill_in :player_2_name, with: 'Alice'
-      click_button('Submit')
-      expect(page).to have_content('Emilie vs. Alice')
+    feature 'Player details' do
+      scenario 'entring names' do
+        visit('/')
+        fill_in :player_1_name, with: 'Roro'
+        fill_in :player_2_name, with: 'champion'
+        click_button('Submit')
+        expect(page).to have_content('Roro vs. champion')
+      end
+    end
+      feature 'Player hit point' do
+        scenario 'view hit points' do
+        visit('/')
+        fill_in :player_1_name, with: 'Roro'
+        fill_in :player_2_name, with: 'champion'
+        click_button('Submit')
+        expect(page).to have_content 'Roro: 10HP'
     end
   end
-# end
+end
