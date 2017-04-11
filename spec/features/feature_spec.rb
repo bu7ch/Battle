22ba681952ffe374b8a0f719bf3d9428d2740992
MrRobot <dutchr@live.fr>
2017-04-11 16:14:44 +0200

    add new feature attack & view, tests OK

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -6,10 +6,18 @@ describe Battle do
         expect(page).to have_content('Roro vs. champion')
       end
     end
-      feature 'Player hit point' do
-        scenario 'view hit points' do
+    feature 'Player hit point' do
+      scenario 'view hit points' do
         sign_in_and_play
         expect(page).to have_content 'Roro: 10HP'
+      end
     end
-  end
+    feature 'Attack player' do
+      scenario 'attack and get confirmation' do
+        sign_in_and_play
+        click_button 'Attack!'
+        expect(page).to have_content 'Roro attacked champion'
+      end
+    end
+
 end
