098889d9ebd9ff02ed48cb01c91e807c828003a2
MrRobot <dutchr@live.fr>
2017-04-11 15:05:17 +0200

    refactor less code smell

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -2,19 +2,13 @@ describe Battle do
 
     feature 'Player details' do
       scenario 'entring names' do
-        visit('/')
-        fill_in :player_1_name, with: 'Roro'
-        fill_in :player_2_name, with: 'champion'
-        click_button('Submit')
+        sign_in_and_play
         expect(page).to have_content('Roro vs. champion')
       end
     end
       feature 'Player hit point' do
         scenario 'view hit points' do
-        visit('/')
-        fill_in :player_1_name, with: 'Roro'
-        fill_in :player_2_name, with: 'champion'
-        click_button('Submit')
+        sign_in_and_play
         expect(page).to have_content 'Roro: 10HP'
     end
   end
